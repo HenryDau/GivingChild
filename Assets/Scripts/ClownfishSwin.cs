832190aa240ac9f6a4a44c5d28b96fca01236530
Henry Dau <hdau@mymail.mines.edu>
2017-06-05 12:20:38 -0600

    Added sea anenononme easter egg

---
+++ Assets/Scripts/ClownfishSwin.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClownfishSwin : MonoBehaviour {
+
+	public int lifetime = 240;
+	private int count = 0;
+	private float startX;
+
+	// Use this for initialization
+	void Start () {
+		startX = transform.position.x;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (count > lifetime)
+			Destroy (gameObject);
+
+		if (!GlobalVariables.isPaused){
+
+			count++;
+
+			transform.position = new Vector3 (
+				startX + (float)(count * 40 / lifetime),
+				transform.position.y,
+				transform.position.z
+			);
+
+			transform.Rotate (Vector3.down, (float)lifetime / 5 / 360);
+		}
+	}
+}
