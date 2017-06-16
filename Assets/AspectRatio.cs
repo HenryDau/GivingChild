24ce0d11185164c70599ca993d25909fe43ca114
Gett Villanueva <gvillanueva@csm-wl-dhcp-231-124.mines.edu>
2017-06-16 16:30:58 -0600

    Added How to Play and moved some buttons around so its not cut off in the screen

---
+++ Assets/AspectRatio.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AspectRatio : MonoBehaviour {
+
+	public float orthoSize = 5;
+	public float aspect = 1.33333f;
+
+	void Start () {
+		Camera.main.projectionMatrix = Matrix4x4.Ortho (
+			-orthoSize * aspect, orthoSize * aspect,
+			-orthoSize, orthoSize,
+			Camera.main.nearClipPlane, Camera.main.farClipPlane);
+	}
+	
+	
+}
